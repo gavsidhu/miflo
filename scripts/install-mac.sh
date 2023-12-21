825c2577a70d5a4b6359b369c9fc7eca0f406742
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-12-21 02:31:48 -0800

    feat: added architecture detection and removed debug snippets

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 set -e
 
-# Define the GitHub repository
 GITHUB_REPO="gavsidhu/miflo"
 
 # Fetch the latest release tag (version) and remove the 'v' prefix
 LATEST_VERSION=$(curl -s https://api.github.com/repos/$GITHUB_REPO/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 
-# Construct the binary URL using the latest version without the 'v'
-BINARY_URL="https://github.com/$GITHUB_REPO/releases/download/v$LATEST_VERSION/miflo_${LATEST_VERSION}_darwin_arm64.tar.gz"
+# Detect the architecture
+ARCH=$(uname -m)
+BINARY_FILE=""
 
-# Debug: Print the URL
-echo "Downloading from: $BINARY_URL"
+case "$ARCH" in
+    "x86_64")
+        # For Intel Macs
+        BINARY_FILE="miflo_${LATEST_VERSION}_darwin_amd64.tar.gz"
+        ;;
+    "arm64")
+        # For Apple Silicon Macs
+        BINARY_FILE="miflo_${LATEST_VERSION}_darwin_arm64.tar.gz"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+BINARY_URL="https://github.com/$GITHUB_REPO/releases/download/v$LATEST_VERSION/$BINARY_FILE"
 
 # Download the binary
 curl -L $BINARY_URL -o miflo.tar.gz
 
-# Debug: Check the downloaded file type
-file miflo.tar.gz
 
 # Extract and move the binary to /usr/local/bin
 tar -xzf miflo.tar.gz
