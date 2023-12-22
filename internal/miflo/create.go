c8e33b3ffb9955c6dd527d23fb98a657cc2c23ff
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-12-21 19:23:04 -0800

    fix: added check for a valid migration name and added error handling for os.Mkdir and os.Create functions

--- internal/miflo/create.go
+++ internal/miflo/create.go
@@ -1,18 +1,34 @@
 package miflo
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path"
+
+	"github.com/gavsidhu/miflo/internal/helpers"
 )
 
 func CreateMigration(migrationName string, cwd string, timestamp int64) error {
 
+	validMigrationName := helpers.IsValidMigrationName(migrationName)
+
+	if !validMigrationName {
+		return errors.New("invalid migration name")
+	}
+
 	pathName := path.Join(cwd, "migrations", fmt.Sprintf("%d_%s", timestamp, migrationName))
 
-	os.Mkdir(pathName, os.ModePerm)
-	os.Create(path.Join(pathName, "up.sql"))
-	os.Create(path.Join(pathName, "down.sql"))
+	if err := os.Mkdir(pathName, os.ModePerm); err != nil {
+		return err
+	}
+	if _, err := os.Create(path.Join(pathName, "up.sql")); err != nil {
+		return err
+	}
+
+	if _, err := os.Create(path.Join(pathName, "down.sql")); err != nil {
+		return err
+	}
 
 	fmt.Println("Migration created successfully")
 
